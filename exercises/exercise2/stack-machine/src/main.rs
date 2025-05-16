eb183e73483daf30eb0baaeb72a2efd6f8e750e6
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-05-16 15:44:22 +0200

    feat(stack-machine): implement stack machine ex2

---
+++ exercises/exercise2/stack-machine/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let mut stack_machine = StackMachine::new();
+    let instructions = vec![
+        Instruction::LOAD(2),
+        Instruction::LOAD(3),
+        Instruction::ADD,
+        Instruction::LOAD(4),
+        Instruction::MUL,
+    ];
+
+    stack_machine.process_instructions(&instructions);
+
+    println!("Result: {:?}", stack_machine.data);
+}
+
+enum Instruction {
+    LOAD(u8),
+    ADD,
+    SUB,
+    MUL,
+}
+
+struct StackMachine {
+    data: Vec<u8>
+}
+
+impl StackMachine {
+    fn new() -> Self {
+        StackMachine { data: Vec::new() }
+    }
+
+    fn process_instructions(&mut self, instructions: &Vec<Instruction>) {
+        for instruction in instructions {
+            match instruction {
+                Instruction::LOAD(value) => self.data.push(*value),
+                Instruction::ADD => {
+                    let a = self.data.pop().unwrap();
+                    let b = self.data.pop().unwrap();
+                    self.data.push(a + b);
+                }
+                Instruction::SUB => {
+                    let a = self.data.pop().unwrap();
+                    let b = self.data.pop().unwrap();
+                    self.data.push(a - b);
+                }
+                Instruction::MUL => {
+                    let a = self.data.pop().unwrap();
+                    let b = self.data.pop().unwrap();
+                    self.data.push(a * b);
+                }
+            }
+        }
+    }
+}
